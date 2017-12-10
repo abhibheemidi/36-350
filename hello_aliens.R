3150da8ae145715df5f3c9df6df1b12ef0bcf826
Abhinav Bheemidi <Abhinav@Abhinavs-MacBook-Pro.local>
2017-12-10 18:13:06 -0500

    Final question!

---
+++ hello_aliens.R
@@ -0,0 +1,5 @@
+cat("Hello Aliens (if that's what you go by)! I am a human living on a planet similar to yours called 'Earth'. 
+    I am reaching out on behalf of humanity to broker an agreement of neutrality between our two planets. 
+    How is life on your planet, and how much variation do you see across different species of flora and fauna? 
+    I'm curious about your planet's way of life given that your days are shorter/longer and 
+    your planet could have multiple moons.")
